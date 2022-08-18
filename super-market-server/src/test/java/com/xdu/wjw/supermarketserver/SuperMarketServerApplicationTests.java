bffe9eb63e71999a375ce8305e66db1378a42453
weijunwei <weijunwei@meituan.com>
2022-08-18 20:46:40 +0800

    func(test):反射test.

--- super-market-server/src/test/java/com/xdu/wjw/supermarketserver/SuperMarketServerApplicationTests.java
+++ super-market-server/src/test/java/com/xdu/wjw/supermarketserver/SuperMarketServerApplicationTests.java
@@ -5,13 +5,16 @@ import com.xdu.wjw.supermarketmodel.model.mapper.UserMapper;
 import com.xdu.wjw.supermarketserver.factory.MetaObjectFactory;
 import com.xdu.wjw.supermarketserver.invoke.lock.DistributeLockService;
 import com.xdu.wjw.supermarketserver.util.CacheUtilService;
-import com.xdu.wjw.supermarketserver.util.LogUtil;
 import org.apache.ibatis.reflection.MetaObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import sun.util.resources.cldr.ss.CalendarData_ss_SZ;
+import org.springframework.util.CollectionUtils;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @SpringBootTest
@@ -54,7 +57,30 @@ class SuperMarketServerApplicationTests {
         System.out.println(metaObject.getObjectWrapper());
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        MetaObjectFactory factory = new MetaObjectFactory();
+        MetaObject metaObject = factory.buildFlowMetaObject(Class.forName("java.lang.String"));
+        Object o = metaObject.getOriginalObject();
+        System.out.println(o);
+        Class<?> clazz = Class.forName("com.xdu.wjw.supermarketserver.util.TestRefil");
+        Method[] methods = clazz.getMethods();
+        Map<String, Map<String, Object>> request = new HashMap<>();
+        HashMap<String, Object> heheRequest = new HashMap<>();
+        heheRequest.put("hehe", "meituan");
+        heheRequest.put("nihao", "weijunwei");
+        request.put("nihao", heheRequest);
+        request.put("hehe", new HashMap<>());
+        Object o1 = clazz.newInstance();
+        for (Method method : methods) {
+            System.out.println(method);
+            Map<String, Object> param = request.get(method.getName());
+            if (!CollectionUtils.isEmpty(param)) {
+                Object[] array = param.values().toArray();
+                for (Object o2 : array) {
+                    System.out.println(o2);
+                }
+                method.invoke(o1, array);
+            }
+        }
     }
 }
